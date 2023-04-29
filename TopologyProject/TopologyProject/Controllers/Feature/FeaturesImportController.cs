54eab2bfcc074fc3823085877c5076b73d788011
Vito Scaletta <lyaguxmaslo@gmail.com>
2023-04-29 11:31:59 +0300

    Separated FeaturesImportController to abstract FeaturesJsonController

---
+++ TopologyProject/TopologyProject/Controllers/Feature/FeaturesImportController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System.Text.Json;
+using IoFile = System.IO.File;
+
+namespace TopologyProject
+{
+    [Route("Features")]
+    public class FeaturesImportController : JsonFeaturesController
+    {
+        public FeaturesImportController(IMemoryCache memoryCache) : base(memoryCache) { }
+
+        [HttpGet]
+        [Route("Import")]
+        public IActionResult SendImportView() => View();
+
+        [HttpPost]
+        public IActionResult Import()
+        {
+            if (TryGetFileFromRequest(HttpContext, out IFormFile? file) == false)
+                return BadRequest();
+
+            string fileContent = GetStringFromFile(file);
+
+            if(TryDeserializeJson(fileContent, out FeatureCollection featureCollection) == false)
+                return BadRequest();
+
+            IoFile.WriteAllText(featuresJsonPath, fileContent);
+
+            WriteJsonToCache(fileContent);
+
+            return Ok();
+        }
+
+        private bool TryGetFileFromRequest(HttpContext context, out IFormFile? file)
+        {
+            file = null;
+
+            var formFiles = context.Request.Form.Files;
+
+            if (formFiles.Any())
+                file = formFiles.First();
+
+            return file is IFormFile;
+        }
+
+        private string GetStringFromFile(IFormFile file)
+        {
+            using (Stream stream = file.OpenReadStream())
+            using (StreamReader reader = new(stream))
+                return reader.ReadToEnd();
+        }
+
+        private bool TryDeserializeJson<T>(string json, out T deserializedObject)
+        {
+            try
+            {
+                deserializedObject = JsonSerializer.Deserialize<T>(json);
+                return true;
+            }
+            catch
+            {
+                deserializedObject = default;
+                return false;
+            }
+        }
+    }
+}
---
+++ TopologyProject/TopologyProject/Controllers/Feature/FeaturesJsonController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System.Text.Json;
+
+namespace TopologyProject
+{
+    public class FeaturesJsonController : Controller
+    {
+        private IMemoryCache _memoryCache;
+
+        public FeaturesJsonController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        public async Task<IActionResult> Export()
+        {
+            byte[] featureJsonBytes = null!;
+
+            if (_memoryCache.TryGetValue(_featureJsonCacheName, out featureJsonBytes) == false)
+            {
+                WriteJsonFromFileInCache(_featureJsonCacheName);
+
+                featureJsonBytes = _memoryCache.Get<byte[]>(_featureJsonCacheName);
+            }
+
+            return File(featureJsonBytes, "application/json", "featuresJson.json");
+        }
+
+        private async void WriteJsonFromFileInCache(string cacheName)
+        {
+            Task<byte[]> featuresJson = System.IO.File.ReadAllBytesAsync(_featuresJsonPath);
+
+            _memoryCache.Set(cacheName, await featuresJson, new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromMinutes(5)));
+        }
+    }
+}
